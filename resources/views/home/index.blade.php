164b185a870a244627a6cd92904b02f4aa469abf
王翊安 <aud986587@gmail.com>
2024-11-26 13:51:25 +0800

    建立master.blade.php，修改index.blade.php

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,4 +1,11 @@
 <!DOCTYPE html>
+@extends('layouts.master')
+@section('title','Home')
+@section('content')
+    <h1>Home</h1>
+    <p>This is my body content.</p>
+@endsection
+
 <html lang="zh">
 <head>
     <meta charset="UTF-8">
